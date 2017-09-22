0c5126382fcc2c3e96cde286638a7f0348f0273a
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-09-22 12:42:26 -0400

    Initialized XCode project.

---
+++ Project_1/Project_1/Project_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project_1
+//
+//  Created by Alessio Mazzone on 9/22/17.
+//  Copyright © 2017 Alessio Mazzone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
