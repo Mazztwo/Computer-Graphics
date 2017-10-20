398aa86b5d3a93186c488c2a4f08b4a317dded63
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-10-20 15:12:54 -0400

    Correctly spins 4 cubes at once with differing speeds/or on different axes.

--- Lab4_Multi_Cube/main.c
+++ Lab4_Multi_Cube/main.c
@@ -31,7 +31,13 @@ Main file for Project 1
 #define BUFFER_OFFSET( offset )   ((GLvoid*) (offset))
 #define windowSize 375
 
-// CUBE ///////////////////////////////////////////////
+Vec4 vertices[36*4];
+Vec4 colors[36*4];
+GLuint ctm_location;
+int num_vertices = 36*4;
+float cubes_123_degrees = 5;
+float cube_4_degrees = 5;
+
 Vec4 cube_vertices[36] =
 {
     // Front Face 1
@@ -90,10 +96,6 @@ Vec4 cube_vertices[36] =
 };
 
 
-Vec4 vertices[36*4];
-Vec4 colors[36*4];
-GLuint ctm_location;
-
 Mat4 cube_tr_matrix[4] =
 {
     {{1.0,0.0,0.0,0.0},
@@ -122,24 +124,23 @@ Mat4 cube_R_matrix[4] =
     {{1.0,0.0,0.0,0.0},
     {0.0,1.0,0.0,0.0},
     {0.0,0.0,1.0,0.0},
-    {0.0,0.0,0.0,1.0}},
+    {-0.5,0.5,0.0,1.0}},
     
     {{1.0,0.0,0.0,0.0},
     {0.0,1.0,0.0,0.0},
     {0.0,0.0,1.0,0.0},
-    {0.0,0.0,0.0,1.0}},
+    {0.5,0.5,0.0,1.0}},
     
     {{1.0,0.0,0.0,0.0},
     {0.0,1.0,0.0,0.0},
     {0.0,0.0,1.0,0.0},
-    {0.0,0.0,0.0,1.0}},
+    {-0.5,-0.5,0.0,1.0}},
     
     {{1.0,0.0,0.0,0.0},
     {0.0,1.0,0.0,0.0},
     {0.0,0.0,1.0,0.0},
-    {0.0,0.0,0.0,1.0}}
+    {0.5,-0.5,0.0,1.0}}
 };
-
 
 
 // Color each face of object
@@ -188,38 +189,21 @@ Vec4 cube_colors[36] =
     {0.0, 1.0, 0.0, 1.0}
 };
 
- 
-// Declare number of verticies
-int num_vertices = 36*4;
 
 void initCubeVertices()
 {
     int i, j, v_index = 0;
     
-    // Transformation matrix here
-    // Cube 1: COM located @ (-0.5,0.5,0.0) and spins about X, fixed pt of rotation is COM
-    // Cube 2: COM located @ (0.5,0.5,0.0) and spins about Y, fixed pt of rotation is COM
-    // Cube 3: COM located @ (-0.5,-0.5,0.0) and spins about Z, fixed pt of rotation is COM
-    // Cube 4: COM located @ (0.5,-0.5,0.0) and spins about X, fixed pt of rotation is COM.
-    //         Make Cube 4 spin faster than Cube 1.
-    // Use IDLE to make cubes spin continuously
-    
     for( i = 0; i < 4; i++)
     {
         for( j = 0; j < 36; j++)
         {
-            //vertices[v_index] = M*cube+vertices[j]
-            //colors[v_index] = ...;
-            //v_index++;
-            
             vertices[v_index] = cube_vertices[j];
             colors[v_index] = cube_colors[j];
             v_index++;
         }
     
     }
-    
-    
 }
 
 void init(void)
@@ -253,22 +237,23 @@ void init(void)
     glDepthRange(1,0);
 }
 
+
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPolygonMode(GL_FRONT, GL_FILL);
     glPolygonMode(GL_BACK, GL_FILL);
     
-    // Cube 1
+    // Draw Cube 1
     glUniformMatrix4fv(ctm_location, 1, GL_FALSE, (GLfloat *) &cube_tr_matrix[0]);
     glDrawArrays(GL_TRIANGLES, 0, 36);
-    // Cube 2
+    // Draw Cube 2
     glUniformMatrix4fv(ctm_location, 1, GL_FALSE, (GLfloat *) &cube_tr_matrix[1]);
     glDrawArrays(GL_TRIANGLES, 36, 36);
-    // Cube 3
+    // Draw Cube 3
     glUniformMatrix4fv(ctm_location, 1, GL_FALSE, (GLfloat *) &cube_tr_matrix[2]);
     glDrawArrays(GL_TRIANGLES, 72, 36);
-    // Cube 4
+    // Draw Cube 4
     glUniformMatrix4fv(ctm_location, 1, GL_FALSE, (GLfloat *) &cube_tr_matrix[3]);
     glDrawArrays(GL_TRIANGLES, 108, 36);
     
@@ -283,89 +268,50 @@ void keyboard(unsigned char key, int mousex, int mousey)
     {
         exit(0);
     }
-    /*
-    // Zoom In
-    else if(key == 'o')
-    {
-        Mat4 tempMatrix = *scaleMatrix(&tr_matrix, 1.02, &tempMatrix);
-        tr_matrix = tempMatrix;
-  
-    }
-    // Zoom Out
-    else if(key == 'l')
-    {
-        Mat4 tempMatrix = *scaleMatrix(&tr_matrix, 1.0/1.02, &tempMatrix);
-        tr_matrix = tempMatrix;
-       
-    }
-    else if(key == 'x')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutX(5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
-       
-    }
-    else if(key == 'X')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutX(-5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
 
-    }
-    else if(key == 'y')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutY(5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
-    
-    }
-    else if(key == 'Y')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutY(-5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
-       
-    }
-    else if(key == 'z')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutZ(5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
-      
-    }
-    else if(key == 'Z')
-    {
-        printf("KEY: %c\n",key);
-        Mat4 rotation = *matRotateAboutZ(-5.0, &rotation);
-        Mat4 tempMatrix2 = *matMultiplication(&rotation, &tr_matrix, &tempMatrix2);
-        tr_matrix = tempMatrix2;
-       
-    }
-    
-    */
     glutPostRedisplay();
     
 }
 
-
+// Have 4 cubes rotate continually by updating their degree
+// of rotation every time idle() is called.
 void idle(void)
 {
+    // Cube 1: COM located @ (-0.5,0.5,0.0) and spins about X, fixed pt of rotation is COM
+    // Cube 2: COM located @ (0.5,0.5,0.0) and spins about Y, fixed pt of rotation is COM
+    // Cube 3: COM located @ (-0.5,-0.5,0.0) and spins about Z, fixed pt of rotation is COM
+    // Cube 4: COM located @ (0.5,-0.5,0.0) and spins about X, fixed pt of rotation is COM.
+    //         Make Cube 4 spin faster than Cube 1.
+    
     // Update Cube 1
-    //Mat4 tempMatrix = *matMultiplication(&R, &tr_matrix,&tempMatrix);
-    //tr_matrix = tempMatrix;
+    Mat4 rotation = *matRotateAboutX(cubes_123_degrees, &rotation);
+    cube_tr_matrix[0] = rotation;
+    Mat4 tempMatrix = *matMultiplication(&cube_R_matrix[0], &cube_tr_matrix[0],&tempMatrix);
+    cube_tr_matrix[0] = tempMatrix;
     
     // Update Cube 2
+    rotation = *matRotateAboutY(cubes_123_degrees, &rotation);
+    cube_tr_matrix[1] = rotation;
+    tempMatrix = *matMultiplication(&cube_R_matrix[1], &cube_tr_matrix[1],&tempMatrix);
+    cube_tr_matrix[1] = tempMatrix;
     
     // Update Cube 3
+    rotation = *matRotateAboutZ(cubes_123_degrees, &rotation);
+    cube_tr_matrix[2] = rotation;
+    tempMatrix = *matMultiplication(&cube_R_matrix[2], &cube_tr_matrix[2],&tempMatrix);
+    cube_tr_matrix[2] = tempMatrix;
     
     // Update Cube 4
+    rotation = *matRotateAboutX(cube_4_degrees, &rotation);
+    cube_tr_matrix[3] = rotation;
+    tempMatrix = *matMultiplication(&cube_R_matrix[3], &cube_tr_matrix[3],&tempMatrix);
+    cube_tr_matrix[3] = tempMatrix;
 
-
+    // Increment the degree of rotation for cubes 1,2,and 3 by 1 degree
+    // and change the degree of cube 4 by 4 degrees to make it spin faster
+    cubes_123_degrees += 1;
+    cube_4_degrees += 4;
+    
     glutPostRedisplay();
 }
 
